3041a13341c92fde5940037fbc47fbca0318017b
lincolnstep <lincolnstepteam@gmail.com>
2019-06-15 08:05:00 +0530

    adding new ui design

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('',views.home,name="home"),
+    path('myblog/', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('about/',views.aboutme,name='about'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,19 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
+def home(request):
+    twoposts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/home.html', {'twoposts':twoposts})
+
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/myblog.html', {'posts': posts})
 
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def aboutme(request):
+    return render(request, 'blog/aboutme.html')
