2011227184625a9b5b122779ca56b9f43bdfdade
lincolnstep <lincolnstepteam@gmail.com>
2019-06-15 15:48:04 +0530

    added ck editor

--- blog/models.py
+++ blog/models.py
@@ -2,12 +2,13 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 from django.core.validators import FileExtensionValidator
+from ckeditor.fields import RichTextField
 
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    text = models.TextField()
+    text = RichTextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
     image = models.ImageField(upload_to='blog_images',validators=[FileExtensionValidator(['jpg','gif','png'])],blank=True)
